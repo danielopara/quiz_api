091d6ab636ce191dfa368da8e6d621876cd6daf8
DanielOpara <oparadaniv@gmail.com>
2025-02-18 21:39:53 +0100

    register userquiz profile

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import AppUser
+from .models import AppUser, UserQuizProfile
 
 # Register your models here.
 admin.site.register(AppUser)
+admin.site.register(UserQuizProfile)
